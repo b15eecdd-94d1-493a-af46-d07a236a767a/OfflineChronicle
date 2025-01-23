4c23c483d29d81e943b9f0ea3b958b6560936eae
b15eecdd-94d1-493a-af46-d07a236a767a <195699994+b15eecdd-94d1-493a-af46-d07a236a767a@users.noreply.github.com>
2025-01-23 19:11:24 +0500

    Add files via upload

--- add.py
+++ add.py
@@ -2,5 +2,14 @@ from blog import Blog
 
 if __name__ == "__main__":
     blog = Blog()
-    text = input('Новая запись: \n')
-    blog.add_post(text)
+    i = 0
+    print("Добавьте новую запись. Используйте Ctrl-D или Ctrl-Z ( windows ) для сохранения.")
+    text = []
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        text.append(line)
+    text = "\n".join(text)
+    blog.add_post(text.strip())
--- list.py
+++ list.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     print("===")
     posts = blog.get_all_posts_with_pagination()
     for post in posts:
-        print(f"ID: {post['id']}:, Дата: {post['date']} \n {post['content']}")
+        print(f"ID: {post['id']}:, Дата: {post['date']} \n{post['content']}")
     print('===')
     print('Количество страниц: ' + str(blog.get_total_pages()))
     print('Количество записей: ' + str(blog.get_total_posts()))
