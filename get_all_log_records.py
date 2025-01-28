f0e7021ab00c84d75cff7ca5c2162210f36c56ce
b15eecdd-94d1-493a-af46-d07a236a767a <195699994+b15eecdd-94d1-493a-af46-d07a236a767a@users.noreply.github.com>
2025-01-28 19:07:54 +0500

    Add files via upload

--- get_all_log_records.py
+++ get_all_log_records.py
@@ -3,14 +3,36 @@ import argparse
 import sys
 import textwrap
 import pprint
+from zoneinfo import ZoneInfo
+from datetime import datetime
+import tzlocal
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("python3 " + sys.argv[0], formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-n", "--name", help="Название блога", type=str, default="blog")
+    parser.add_argument("-tz", "--timezone", help="Часовой пояс (local - локальный часовой пояс)", type=str, default="local")
+    parser.add_argument("-sep", "--separator", help="Разделитель между постами", type=str, default="-" * 40)
+    parser.add_argument("-s", "--sort", help="Сортировка", type=str, choices=["DESC", "ASC"], default="ASC")
     args = parser.parse_args()
     # Получаем все записи
     blog = Blog(args.name)
-    all_log_records = blog.get_all_log_records()
+    all_log_records = blog.get_all_log_records(sort = args.sort)
     all_log_records = [dict(log_record) for log_record in all_log_records]
-    pprint.pp(all_log_records)
+    for log_record in all_log_records:
+        print("|" + args.separator)
+        if log_record['timezone'] == None:
+            origin_tz = ZoneInfo(tzlocal.get_localzone_name())
+        else:
+            origin_tz = ZoneInfo(str(log_record['timezone']))
+        dt = datetime.fromisoformat(log_record['date']).replace(tzinfo=origin_tz)
+        if args.timezone == "local" or args.timezone == "":
+            dt = dt.astimezone(tz=ZoneInfo(tzlocal.get_localzone_name()))
+        else: 
+            dt = dt.astimezone(tz=ZoneInfo(args.timezone))
+        print(f"|ID: {log_record['id']}:, Дата: {dt}")
+        print("|" + args.separator)
+        print(f"|Событие: " + log_record['action'] + f", пост №{log_record['post_id']}")
+        print("|" + "-" * 40)
+        print("|")
+    print('Записей: ' + str(len(all_log_records)))
     blog.close() 
--- search.py
+++ search.py
@@ -2,6 +2,9 @@ from lib.blog import Blog
 import argparse
 import sys
 import textwrap
+from datetime import datetime
+from zoneinfo import ZoneInfo
+import tzlocal
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("python3 " + sys.argv[0], formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -9,18 +12,32 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--sort", help="Сортировка", type=str, choices=["DESC", "ASC"], default="DESC")
     parser.add_argument("-sep", "--separator", help="Разделитель между постами", type=str, default="-" * 40)
     parser.add_argument("-w", "--width", help="Максимальная ширина строки", type=int, default=40)
+    parser.add_argument("-tz", "--timezone", help="Часовой пояс (local - локальный часовой пояс)", type=str, default="local")
     args = parser.parse_args()
     # Получаем записи
-    blog = Blog(args.name)
+    blog = Blog(args.name, sort=args.sort)
     search_text = input('Строка для поиска: ')
     
     posts = blog.search_posts(search_text)
+    
     for post in posts:
         print("|" + args.separator)
-        print(f"|ID: {post['id']}:, Дата: {post['date']}")
+        if post['timezone'] == None:
+            origin_tz = ZoneInfo(tzlocal.get_localzone_name())
+        else:
+            origin_tz = ZoneInfo(str(post['timezone']))
+        dt = datetime.fromisoformat(post['date']).replace(tzinfo=origin_tz)
+        if args.timezone == "local" or args.timezone == "":
+            dt = dt.astimezone(tz=ZoneInfo(tzlocal.get_localzone_name()))
+        else: 
+            dt = dt.astimezone(tz=ZoneInfo(args.timezone))
+        print(f"|ID: {post['id']}:, Дата: {dt}")
         print("|" + args.separator)
-        content = textwrap.fill(str(post['content']), args.width)
+        if post['title'] != None and post['title'].strip() != "": 
+            print("|" + post['title'])
+        content = textwrap.fill(str(post['content']), args.width, replace_whitespace = False)
         print("|" + "\n|".join(content.split('\n')))
         print("|" + "-" * 40)
         print("|")
+    print('Количество записей: ' + str(len(posts)))
     blog.close() 
